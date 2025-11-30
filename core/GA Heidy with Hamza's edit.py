96b6e0ac3ba328807b14a4259f81351af0ffd34f
HaidySalem <haidysalem2409@gmail.com>
2025-11-30 14:16:21 +0200

    Update GA Heidy with Hamza's edit.py
    1.changes fitness dic into list
    2. random selection
    3. rollute wheel selection

--- core/GA Heidy with Hamza's edit.py
+++ core/GA Heidy with Hamza's edit.py
@@ -1,6 +1,7 @@
 # Heidy Salem
 # 20230641
 # GA for feature selection
+from re import S
 import numpy as np
 import array
 
@@ -31,6 +32,7 @@ def initialize_population(population_size: int , num_features: int):
 
 # the function returns a list of random chromosomes (n number of chromosomes = 20 which is the population size and each chromosome is an array of binary bits (0,1) of size 12 which is the number of features in the dataset)
 # the function uses a list comprehension to create a list of random chromosomes
+
 population = initialize_population(population_size=20, num_features=12)
 for chromo in population:
     print(chromo)
@@ -48,7 +50,7 @@ from sklearn.tree import DecisionTreeClassifier
 # the function uses the cross_val_score function to get the accuracy of the model
 # the function returns the fitness of the chromosome
 # fitness formula:
-# fitness=accuracy−Beta⋅( Features_selected / Features_total  ) 
+# fitness=accuracy*alpha−Beta⋅( Features_selected / Features_total  ) 
 #Fitness Function for One Chromosome
 
 def compute_fitness(chromosome, Dataset_features, prediction_target, alpha=1,  Beta=1):
@@ -76,7 +78,7 @@ def compute_fitness(chromosome, Dataset_features, prediction_target, alpha=1,  B
     accuracy = np.mean(cross_val_score(model, Dataset_features_selected, prediction_target, cv=3))
 
     # Penalty
-    Features_total = len(chromosome)
+    Features_total = len(chromosome) #  = len(Dataset_features)
     Features_selected = len(selected_indices)
     penalty = Beta * (Features_selected / Features_total)
 
@@ -84,7 +86,7 @@ def compute_fitness(chromosome, Dataset_features, prediction_target, alpha=1,  B
     return fitness_value
 
 # Function to Calculate Fitness for Whole Population
-# Returns a dictionary of chromosome and value of each fitness for this chromosome
+# Returns a List of chromosome and value of each fitness for this chromosome
 #-> a function  evaluate population take the array population and call compute fitness for each chromosome in the population and return a dictionary of key index chromosome and value of each fitness for this chromosome   
 # the function gets population, Dataset_features, prediction_target, Beta as an input and returns a dictionary of chromosome and value of each fitness for this chromosome
 # the function uses a for loop to iterate over the population
@@ -92,18 +94,17 @@ def compute_fitness(chromosome, Dataset_features, prediction_target, alpha=1,  B
 # the function returns a dictionary of chromosome and value of each fitness for this chromosome
 # Function to Calculate Fitness for Whole Population
 
-def evaluate_population(population, Dataset_features, prediction_target, Beta=1):
-    fitness_dict = {}
+def get_fitness_list(population, Dataset_features, prediction_target):
+    """
+    Returns a list of fitness values for each chromosome in the population.
+    """
+    fitness_list = []
 
-    for index, chromosome in enumerate(population):
-        fitness = compute_fitness(chromosome, Dataset_features, prediction_target, Beta)
-        fitness_dict[index] = {
-            "chromosome": chromosome,
-            "fitness": fitness
-            }
-        # the function uses a dictionary to store the chromosome and the fitness of the chromosome
-    return fitness_dict
-# the function returns a dictionary of chromosome and value of each fitness for this chromosome
+    for chromosome in population:
+        fitness = compute_fitness(chromosome, Dataset_features, prediction_target, alpha=1, Beta=1)
+        fitness_list.append(fitness)
+
+    return fitness_list
 
 import pandas as pd
 # Importing the dataset
@@ -122,6 +123,172 @@ categorical_cols = Dataset_features.select_dtypes(include='object').columns
 Dataset_features = pd.get_dummies(Dataset_features, columns=categorical_cols, drop_first=True)
 
 population = initialize_population(population_size=20, num_features=len(Dataset_features.columns))
-fitness_results = evaluate_population(population, Dataset_features, prediction_target)
 
-print(fitness_results)
+fitness_list = get_fitness_list(population, Dataset_features, prediction_target)
+
+print(fitness_list)
+
+
+
+# Genetic Algorithm Selection methods 
+# first random parents Selection method (the easiest method for selection)
+
+# Version 1 — Select 2 random parents
+# the function gets population as an input and returns 2 random parents
+# the function uses the random.choice function to select 2 random parents
+# the function returns 2 random parents (parent1 and parent2)
+
+import random
+
+def random_selection(population):
+    parent1 = random.choice(population)#fun random.choice -> 
+    parent2 = random.choice(population)
+    return parent1, parent2
+
+p1, p2 = random_selection(population)
+print(p1)
+print(p2)
+
+def random_selection_2(population, k=2):
+    return random.sample(population, 2)# -> Note:
+ # random.sample chooses unique parents (no repetition).
+
+p1, p2 = random_selection_2(population)
+print(p1)
+print(p2)
+
+    #----------------------------------------------#
+    #Version 2 — Select k parents
+# the function gets population and k as an input and returns k random parents
+# the function uses the random.sample function to select k random parents
+# the function returns k random parents
+
+def random_selection_k(population, k):
+    return random.sample(population, k)# -> Note:
+# random.sample chooses unique parents (no repetition).
+
+p1, p2, p3, p4 = random_selection_k(population,4)
+print(p1)
+print(p2)
+print(p3)
+print(p4)
+
+#----------------------------------------------#
+
+# Second. Roulette Wheel Selection (Fitness Proportionate)
+#Idea :
+# Chromosomes with higher fitness get bigger slice of the wheel.
+#Selection probability = fitness / total_fitness.
+
+####################   ⚠ Important:    ####################
+# Fitness must be positive. If not, you must shift values.
+#
+# Shift Fitness Values (to be positive)
+
+
+# Shift fitness to ensure all values are positive
+def shift_fitness(fitness_list):
+    # Extract fitness values from dict
+    fitnesses = [fitness_list[i]["fitness"] for i in fitness_list]
+
+    min_f = min(fitness_list)
+
+    # Shift values only if needed
+    if min_f <= 0:
+        shift_amount = abs(min_f) + 1e-6
+        fitnesses = [f + shift_amount for f in fitness_list]
+
+    return fitnesses
+
+fitnesses = shift_fitness(fitness_list)
+# Now we can do : Roulette Wheel Selection (Fitness Proportionate) Safetly
+
+
+# Function: Descending_order_fitnesses
+def Descending_order_fitnesses(fitnesses):
+    """
+    Returns:
+        list1: fitness values sorted in descending order
+        list2: indices of chromosomes arranged according to sorted fitness
+    """
+
+    # Pair index with fitness
+    indexed_fitness = list(enumerate(fitnesses))  #fitnesses =[30,20,40]
+    #Ex:
+    # indexed_fitness = [(0,30), (1,20), (2,40)]
+
+    # Sort by fitness DESC (key = fitness)
+    sorted_pairs = sorted(indexed_fitness, key=lambda x: x[1], reverse=True)
+    #Ex:
+    # sorted_pairs = [(2,40), (0,30), (1,20)]
+
+    # des_order_fitnesses: sorted fitness values
+    des_order_fitnesses = [pair[1] for pair in sorted_pairs]
+    #des_order_fitnesses = [40,30,20]
+
+    # des_order_indices: original indices arranged based on sorted fitness
+    des_order_indices = [pair[0] for pair in sorted_pairs]
+    # des_order_indices: [2,0,1]
+
+    return des_order_fitnesses, des_order_indices
+
+list1, list2 = Descending_order_fitnesses(fitnesses)
+
+print("Descending Fitness:", list1)
+print("Sorted Indexes:", list2)
+
+# Function: Descending_order_ratios :
+def Descending_order_ratios(list1, fitnesses): #list1 =[0.5,0.3,0.2] 
+    total_fitness = sum(fitnesses)#1
+    ratios =[]
+    for fit in list1:
+        ratio =(fit / total_fitness)*100 # -> 0.5/1*100=50
+        ratios.append(ratio) 
+    
+    return ratios #[50,30,20]
+
+des_r = Descending_order_ratios(list1, fitnesses)
+
+def roulette_wheel(ratios):
+    """
+    Given a list of ratios (probabilities),
+    return the cumulative roulette wheel list.
+    Example:
+        [50, 30, 20] -> [50, 80, 100]
+    """
+
+    roulette = []
+    cumulative_sum = 0
+
+    for r in ratios:
+        cumulative_sum += r
+        roulette.append(cumulative_sum)
+
+    return roulette  #->-> [50, 80, 100]
+
+
+ratios = Descending_order_ratios(list1, fitnesses)
+roulette_list = roulette_wheel(ratios)
+
+print(roulette_list)
+
+import random
+
+def roulette_wheel_selection(roulette_list, list2, population):
+    # random number between 0 and 100
+    r = random.uniform(0, 100) #->70
+
+    # Find the first interval where r <= roulette_list[i]
+    for i in range(len(roulette_list)): 
+        if r <= roulette_list[i] :    
+            selected_index = list2[i]          # original index in population
+            return population[selected_index]  # return the chromosome
+        
+
+selected = roulette_wheel_selection(roulette_list, list2, population)
+print(selected)
+
+################################################################################################################### 
+###################################################################################################################
+###################################################################################################################
+###################################################################################################################
