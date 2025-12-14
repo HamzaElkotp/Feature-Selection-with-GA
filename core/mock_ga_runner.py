5a9a8ed1112889e8cc8209918ebaf25cfd9b2a8b
HamzaDev <hmzt25235@gmail.com>
2025-12-14 21:05:57 +0200

    connect mock with runner

--- core/mock_ga_runner.py
+++ core/mock_ga_runner.py
@@ -48,23 +48,37 @@ rotation_mutator,
 
 class MockGAService(GAInterface):
 
-    def run_ga(self, parms: RunGAParameters, on_complete: Optional[Callable[[Merged_GA], None]] = None) -> None:
+    def run_ga(self, params: RunGAParameters, on_complete: Optional[Callable[[Merged_GA], None]] = None) -> None:
         new_Ga = GA(
-            initiate_population=initialize_population,
-            elitism=elitism_selector,
-            selection=tournament_selection,
-            crossover=population_k_point_crossover,
-            mutation=complement_mutator,
-            compute_generation_fitness=get_population_fitness,
-            dataset_path=r"D:\SelfAcademicLearn\University\Year 3\AI\Project\processed_Breast_Cancer_Dataset.csv",
-            result_col_name="diagnosis",
-            num_generations=4,
-            population_size=20,
-            crossover_k_points=2,
-            mutation_percent=4,
-            elitism_percent=1,
-            alpha=1,
-            beta=1,
+            initiate_population=initialize_population, # function call
+            elitism=elitism_selector, # function call
+            selection = (
+                tournament_selection
+                if params.ga_parameters.selection_method == SelectionMethod.TOURNAMENT
+                else roulette_wheel_selection
+                if params.ga_parameters.selection_method == SelectionMethod.ROULETTE
+                else random_selection_unique
+            ), # function call
+            crossover=population_k_point_crossover, # function call
+            mutation= (
+                bit_flip_mutator
+                if params.ga_parameters.mutation_method == MutationMethod.Bit_Flip
+                else complement_mutator
+                if params.ga_parameters.mutation_method == MutationMethod.Bit_String_Complement
+                else reverse_mutator
+                if params.ga_parameters.mutation_method == MutationMethod.Bit_String_Reverse
+                else rotation_mutator
+            ), # function call
+            compute_generation_fitness=get_population_fitness, # function call
+            dataset_path=params.dataset_file_path,
+            result_col_name=params.ga_parameters.result_col_name,
+            num_generations=params.ga_parameters.num_of_generations,
+            population_size=params.ga_parameters.initial_population_size,
+            crossover_k_points=params.ga_parameters.crossover_k_points,
+            mutation_percent=params.ga_parameters.mutation_percent,
+            elitism_percent=params.ga_parameters.elitism_percent,
+            alpha=params.ga_parameters.alpha,
+            beta=params.ga_parameters.beta,
         )
 
         result:Merged_GA = new_Ga.master_run(num_runs=20) # type: ignore
